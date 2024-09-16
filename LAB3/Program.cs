2ec8bc43c8bb83bb2169d3b0e2647ba77ace4a27
Lucas Brown <lmbrown19@crimson.ua.edu>
2024-09-16 09:23:09 -0500

    LAB3

---
+++ LAB3/Program.cs
@@ -0,0 +1,55 @@
+﻿
+//main
+string enjoymentLevel = GetEnjoymentLevel();
+string stadium = GetStaduimReccomendation(enjoymentLevel);
+string game = GetGameReccomendation(stadium);
+DisplayStadiumDetails(stadium , game);
+//end main
+
+static string GetEnjoymentLevel(){
+    System.Console.WriteLine("What is your preferred enjoyment level?\n Boring \n Average \n Fun \n Epic");
+    System.Console.WriteLine("Enter 1 for boring, 2 for Average, 3 for Fun, 4 for Epic");
+    return (Console.ReadLine());
+}
+
+static string GetStaduimReccomendation(string enjoymentLevel){
+    if(enjoymentLevel == "1"){
+        return "Neyland stadium";
+    }
+    else if(enjoymentLevel == "2"){
+        return "Jordan-Hare Stadium";   
+    }
+    else if(enjoymentLevel == "3"){
+        return "Tiger Stadium";    
+    }
+    else if(enjoymentLevel == "4"){
+        return "Saban Field at Bryant-Denny Stadium";
+    }
+    else{
+        return "INVALID INPUT";
+    }
+}
+
+static string GetGameReccomendation(string stadium){
+    if(stadium == "Neyland stadium"){
+        return "Tennesse vs Kent state";
+    }
+    else if(stadium == "Jordan-Hare Stadium"){
+        return "Auburn vs Kentucky";
+    }
+    else if(stadium == "Tiger Stadium"){
+        return "LSU vs Alabama";
+    }
+    else if(stadium == "Saban Field at Bryant-Denny Stadium"){
+        return "Alabama vs Auburn";
+    }
+    else{
+        return "INVALID";
+    }
+}
+
+static void DisplayStadiumDetails(string stadium, string game){
+    Console.Clear();
+    System.Console.WriteLine($"We reccomend you go to {stadium} to watch {game}");
+}
+
